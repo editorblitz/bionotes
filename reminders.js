b48ef2f595ff9abd1fc040c022806a8156f8f719
editorblitz <67771494+editorblitz@users.noreply.github.com>
2023-05-20 21:59:29 -0400

    Create reminders.js

---
+++ reminders.js
@@ -0,0 +1,78 @@
+[
+  {
+    categoryName: 'Medicines',
+    groups: [
+      {
+        group: 'CAR-T',
+        items: [
+          {
+            item: 'Kymriah',
+            description: 'First CAR-T approved in U.S.',
+            details: {
+              Maker: 'Novartis',
+              Notes: 'Approved in 2017. Initially approved for pediatric and young adult patients with a form of acute lymphoblastic leukemia (ALL). Later it also got approved for adult patients with relapsed or refractory (r/r) large B-cell lymphoma after two or more lines of systemic therapy.'
+            }
+          },
+          {
+            item: 'Yescarta',
+            description: 'Second CAR-T approved in U.S.',
+            details: {
+              Maker: 'Gilead Sciences',
+              Notes: 'Approved in 2017. It is approved for adult patients with relapsed or refractory large B-cell lymphoma after two or more lines of systemic therapy.'
+            }
+          },
+        ]
+      },
+			{
+        group: 'RSV vaccines',
+        items: [
+          {
+            item: 'Kymriah',
+            description: 'First CAR-T approved in U.S.',
+            details: {
+              Maker: 'Novartis',
+              Notes: 'Approved in 2017. Initially approved for pediatric and young adult patients with a form of acute lymphoblastic leukemia (ALL). Later it also got approved for adult patients with relapsed or refractory (r/r) large B-cell lymphoma after two or more lines of systemic therapy.'
+            }
+          },
+          {
+            item: 'Yescarta',
+            description: 'Second CAR-T approved in U.S.',
+            details: {
+              Maker: 'Gilead Sciences',
+              Notes: 'Approved in 2017. It is approved for adult patients with relapsed or refractory large B-cell lymphoma after two or more lines of systemic therapy.'
+            }
+          },
+        ]
+      },
+      // additional groups could go here
+    ]
+  },
+	{
+    categoryName: 'Companies',
+    groups: [
+      {
+        group: 'Pfizer',
+        items: [
+          {
+            item: 'Covid vaccine',
+            description: 'First CAR-T approved in U.S.',
+            details: {
+              Maker: 'Novartis',
+              Notes: 'Approved in 2017. Initially approved for pediatric and young adult patients with a form of acute lymphoblastic leukemia (ALL). Later it also got approved for adult patients with relapsed or refractory (r/r) large B-cell lymphoma after two or more lines of systemic therapy.'
+            }
+          },
+          {
+            item: 'RSV vaccine',
+            description: 'Second CAR-T approved in U.S.',
+            details: {
+              Maker: 'Gilead Sciences',
+              Notes: 'Approved in 2017. It is approved for adult patients with relapsed or refractory large B-cell lymphoma after two or more lines of systemic therapy.'
+            }
+          },
+        ]
+      },
+      // additional groups could go here
+    ]
+  },
+  // additional categories could go here
+];
